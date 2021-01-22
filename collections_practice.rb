d2f1d992a7a8dc59be96551b95e2df75f6124a18
rtyenriques <tooshopboys@gmail.com>
2021-01-21 21:28:14 -0800

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,56 +1,64 @@
-
 def sort_array_asc(array)
-    array.sort do |a, b|
-      a <=> b
-    end
+  array.sort
+end
+
+# def sort_array_desc(array)
+#   array.sort do |a,b|
+#     if a == b
+#       0
+#     elsif a < b
+#       1
+#     else a > b
+#       -1
+#     end
+#   end
+# end
+
+def sort_array_desc(array)
+  array.sort do |left, right|
+   right <=> left
   end
-  
-  def sort_array_desc(array)
-    array.sort do |left, right|
-     right <=> left
-    end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
   end
-  
-  def sort_array_char_count(array)
-    array.sort do |left, right|
-      left.length <=> right.length
-    end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[2] <=> b[1]
   end
-  
-  def swap_elements(array)
-    array.sort do |left, right|
-      left[2] <=> right [1]
-    end
-  end
-  
-  def reverse_array(array)
+end
+
+def reverse_array(array)
   array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
   end
-  
-  def kesha_maker(array)
-    array.each do |item|
-      item[2] = "$"
+end
+
+def find_a(array)
+  array.find_all do |fruit|
+    fruit[0] == "a"
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.inject(0) {|sum, num| sum+num}
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
     end
   end
-  
-  def find_a(array)
-    array.find_all do |word|
-      word[0] == "a"
-    end
-  end
-  
-  def sum_array(array)
-    sum = 0
-  array.inject(0) {|sum, num| sum + num}
-  end
-  
-  def add_s(array)
-    array.collect do |word|
-      if array[1] == word
-        word
-      else
-        word + "s"
-      end
-    end
-  end
-  
+end
